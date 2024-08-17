a6953eb94589a64d0b8bbddd506b9cb4d7f1657a
Abdul Hameed <sd.hameed79@gmail.com>
2024-08-17 18:15:27 +0530

    Read More buttton added for card description

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,23 +1,38 @@
 // src/components/TourCard.jsx
-import React from 'react';
+import React, { useState } from 'react';
 
 const TourCard = ({ place, onConfirm, onNotInterested }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleReadMore = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div className="bg-white rounded-lg shadow-md p-4 mb-4 transform transition duration-300 hover:scale-105 hover:shadow-xl hover:shadow-stone-400">
-      <img src={place.image} alt={place.destination} className="w-full h-52 object-fit rounded-md" />
+    <div className="bg-white rounded-lg shadow-md p-4 mb-4 transform transition duration-300 hover:scale-105 hover:shadow-xl hover:shadow-zinc-500">
+      <img src={place.image} alt={place.destination} className="w-full h-52 object-fill rounded-md" />
       <h3 className="text-xl font-bold mt-4">{place.destination}</h3>
-      <p className="text-gray-600 mt-2 overflow-x-hidden">{place.description}</p>
+      <div className="mt-2">
+        <p className="text-gray-600 inline">
+          {isExpanded ? place.Description : `${place.Description.slice(0, 80)}...`}
+        </p>
+        <button 
+          className="text-blue-400 hover:text-blue-600 ml-2" 
+          onClick={toggleReadMore}>
+          {isExpanded ? 'Read Less' : 'Read More'}
+        </button>
+      </div>
       <p className="text-lg font-semibold mt-2">${place.price}</p>
       <div className="mt-4 flex justify-between">
         <button 
-          className="text-green-500 border border-green-500 px-4 py-2 rounded-xl hover:bg-green-600 hover:text-white hover:scale-90 transition duration-300" 
+          className="text-lg text-green-500 border border-green-500 hover:text-white px-4 py-2 rounded-md hover:bg-green-600 transition duration-300 hover:scale-90" 
           onClick={() => onConfirm(place.id)}>
-          Intrested
+          Interested
         </button>
         <button 
-          className="text-red-500 border border-red-500 hover:text-white px-4 py-2 rounded-xl hover:bg-red-600 hover:scale-90 transition duration-300" 
+          className="text-red-500 border border-red-500 hover:text-white px-4 py-2 rounded-md hover:bg-red-600 transition duration-300 hover:scale-90" 
           onClick={() => onNotInterested(place.id)}>
-          Not Intrested
+          Not Interested
         </button>
       </div>
     </div>
