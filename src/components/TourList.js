15af74e15a8dd85e11165af4974d04e2c061931b
Abdul Hameed <sd.hameed79@gmail.com>
2024-08-17 13:47:13 +0530

    Update UI flow

--- src/components/TourList.js
+++ src/components/TourList.js
@@ -41,7 +41,7 @@ const TourList = ({ places }) => {
   }
 
   return (
-    <div className="w-3/4 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="flex-wrap grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {remainingPlaces.map((place) => (
         <TourCard 
           key={place.id} 
