f80f2844fb28f9d3982415f62ce06b24fedcd515
Abdul Hameed <sd.hameed79@gmail.com>
2024-08-21 17:08:31 +0530

    initial commit

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -9,11 +9,11 @@ const TourCard = ({ place, onConfirm, onNotInterested }) => {
   };
 
   return (
-    <div className="bg-gray-300 shadow-md p-4 mb-4 transform transition duration-300 hover:scale-105 hover:shadow-xl hover:shadow-slate-400 flex-wrap">
+    <div className="bg-black rounded-lg shadow-md p-4 mb-4 transform transition duration-300 hover:scale-105 hover:shadow-xl hover:shadow-black flex-wrap">
       <img src={place.image} alt={place.name} className="w-96 h-52 object-fill rounded-md" />
-      <h3 className="text-xl font-bold mt-4">{place.name}</h3>
+      <h3 className="text-xl text-white font-bold mt-4">{place.name}</h3>
       <div className="mt-2">
-        <p className="text-gray-600 inline">
+        <p className="text-slate-300 inline">
           {isExpanded ? place.Description : `${place.Description.slice(0, 80)}...`}
         </p>
         <button 
@@ -22,7 +22,7 @@ const TourCard = ({ place, onConfirm, onNotInterested }) => {
           {isExpanded ? 'Read Less' : 'Read More'}
         </button>
       </div>
-      <p className="text-lg font-semibold mt-2">${place.price}</p>
+      <p className="text-lg text-white font-semibold mt-2">${place.price}</p>
       <div className="mt-4 flex justify-between">
         <button 
           className="text-lg text-green-500 border border-green-500 hover:text-white px-4 py-2 rounded-md hover:bg-green-600 transition duration-300 hover:scale-90" 
