51d39513226649323dacc2c9b0cf50cfab40dbef
Abdul Hameed <sd.hameed79@gmail.com>
2024-08-22 13:00:51 +0530

    hover effects added to the buttons

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -1,7 +1,7 @@
 // src/components/TourCard.jsx
 import React, { useState } from 'react';
 
-const TourCard = ({ place, onConfirm, onNotInterested }) => {
+const TourCard = ({ place, onIntrested, onNotInterested }) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   const toggleReadMore = () => {
@@ -10,7 +10,7 @@ const TourCard = ({ place, onConfirm, onNotInterested }) => {
 
   return (
     <div className="bg-black rounded-lg shadow-md p-4 mb-4 transform transition duration-300 hover:scale-105 hover:shadow-xl hover:shadow-black flex-wrap">
-      <img src={place.image} alt={place.name} className="w-96 h-52 object-fill rounded-md" />
+      <img src={place.image} alt={place.name} className="w-96 h-52 object-cover rounded-md" />
       <h3 className="text-xl text-white font-bold mt-4">{place.name}</h3>
       <div className="mt-2">
         <p className="text-slate-300 inline">
@@ -23,10 +23,10 @@ const TourCard = ({ place, onConfirm, onNotInterested }) => {
         </button>
       </div>
       <p className="text-lg text-white font-semibold mt-2">${place.price}</p>
-      <div className="mt-4 flex justify-between">
+      <div className="mt-4 flex flex-wrap justify-between">
         <button 
           className="text-lg text-green-500 border border-green-500 hover:text-white px-4 py-2 rounded-md hover:bg-green-600 transition duration-300 hover:scale-90" 
-          onClick={() => onConfirm(place.id)}>
+          onClick={() => onIntrested(place.id)}>
           Interested
         </button>
         <button 
