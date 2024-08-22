5163e555ca1623424c299afe1fbac866a2bfac77
Abdul Hameed <sd.hameed79@gmail.com>
2024-08-22 12:55:37 +0530

    UI flow changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import places from './Data.js';
 const App = () => {
   return (
     <div className="container mx-auto my-6">
-      <h1 className='text-center text-slate-200 text-3xl mb-10 font-extrabold'> 
-        Select Your Destiny 
-      </h1>
         <TourList places={places} />
       </div>
     );
